06e1c9bfaa887fb7196eccf301f3fa3e274985f6
Craig Hockenberry <craig.hockenberry@gmail.com>
2025-01-30 11:34:47 -0800

    Initial Commit

---
+++ BlueskyOAuth/BlueskyOAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueskyOAuthApp.swift
+//  BlueskyOAuth
+//
+//  Created by Craig Hockenberry on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BlueskyOAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
